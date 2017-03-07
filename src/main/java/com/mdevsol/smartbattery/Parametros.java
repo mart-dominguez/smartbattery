ce9a66119c6c973cb6545ba06522e38dda025e32
Martin Dominguez <mdominguez@santafe.gov.ar>
2017-03-07 13:01:30 -0300

    primer commit

---
+++ src/main/java/com/mdevsol/smartbattery/Parametros.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mdevsol.smartbattery;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author mdominguez
+ */
+@Entity
+@Table(name = "SB_PARAMETROS")
+public class Parametros {
+
+    @Id
+    private Integer id;
+    @Column(name="NOMBRE")
+    private String nombre;
+    @Column(name="DESCRIPCION")
+    private String descripcion;
+    @Column(name="UNIDADES")
+    private String unidades;
+    @Column(name="VALOR")
+    private Double valor;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(String unidades) {
+        this.unidades = unidades;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    
+}
